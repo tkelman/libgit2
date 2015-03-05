b97476c6281e71695a08b2250b218426648e18c5
Tony Kelman <tony@kelman.net>
2015-03-04 16:54:27 -0800

    install openssl on appveyor

--- script/appveyor-mingw.sh
+++ script/appveyor-mingw.sh
@@ -3,6 +3,7 @@ set -e
 cd `dirname "$0"`/..
 if [ "$ARCH" = "32" ]; then
   echo 'C:\MinGW\ /MinGW' > /etc/fstab
+  openssl=Win32OpenSSL-1_0_2.exe
 elif [ "$ARCH" = "i686" ]; then
   f=i686-4.9.2-release-win32-sjlj-rt_v3-rev1.7z
   if ! [ -e $f ]; then
@@ -10,6 +11,7 @@ elif [ "$ARCH" = "i686" ]; then
   fi
   7z x $f > /dev/null
   mv mingw32 /MinGW
+  openssl=Win32OpenSSL-1_0_2.exe
 else
   f=x86_64-4.9.2-release-win32-seh-rt_v3-rev1.7z
   if ! [ -e $f ]; then
@@ -17,7 +19,12 @@ else
   fi
   7z x $f > /dev/null
   mv mingw64 /MinGW
+  openssl=Win64OpenSSL-1_0_2.exe
 fi
+if ! [ -e $openssl ]; then
+  curl -LsSO http://slproweb.com/download/$openssl
+fi
+$openssl //silent //verysilent //sp- //suppressmsgboxes //DIR="C:\\OpenSSL"
 cd build
-cmake -D ENABLE_TRACE=ON -D BUILD_CLAR=ON .. -G"$GENERATOR"
+cmake -D ENABLE_TRACE=ON -D BUILD_CLAR=ON -D OPENSSL_INCLUDE_DIR="C:\\OpenSSL\include" .. -G"$GENERATOR"
 cmake --build . --config RelWithDebInfo
